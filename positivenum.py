2593af9467b4064f8776abf7efe72065c30c54d0
Shrirang-Zend <128277571+Shrirang-Zend@users.noreply.github.com>
2023-04-12 20:44:04 +0530

    Add files via upload

---
+++ positivenum.py
@@ -0,0 +1,13 @@
+NumList1 = [12, -7, 5, 64, -14]
+
+for num in NumList1:
+    if num>0:
+        print(num)
+        
+        
+NumList2 = [12, 14, -95, 3]
+
+for num in NumList2:
+    if num>0:
+        print(num)
+    
